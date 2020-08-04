5f126f55a14945dbac8887fb481bfbbc44f26793
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-08-04 20:48:47 +0200

    Update projects contentText

---
+++ JS/GenerateProjects.js
@@ -0,0 +1,9 @@
+const sectionDataUrl = "./Data/ProjectsData.json"
+
+function renderProjectData(jsonString) {
+    let projectData = JSON.parse(jsonString);
+    
+    projectData.Projects.forEach(Project => {
+        
+    })
+}
