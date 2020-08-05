af502b3dde114f186f62a81b27cfdafa5744e795
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-08-05 13:02:27 +0200

    Fix the cannot find makeAjaxCall function

--- JS/GenerateProjects.js
+++ JS/GenerateProjects.js
@@ -1,9 +1,17 @@
-const sectionDataUrl = "./Data/ProjectsData.json"
+const projectDataUrl = "../Data/ProjectsData.json"
 
 function renderProjectData(jsonString) {
     let projectData = JSON.parse(jsonString);
-    
+
+    let projectsContainer = document.getElementById("ProjectsContainer");
+
     projectData.Projects.forEach(Project => {
-        
+        let projectTitle = document.createElement("div");
+        projectTitle.className = "projects__title";
+        projectTitle.innerHTML = Project.Title;
+        projectsContainer.appendChild(projectTitle);
     })
 }
+
+// makeAjaxCall("GET", projectDataUrl, renderProjectData);
+makeAjaxCall("GET", projectDataUrl, renderProjectData);
