bb34efb5cd89bd817b2ac212864ce388bc0a5849
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-11-24 11:37:17 +0100

    ProjectContainer clears itself when GenerateProjectData function is called

--- JS/GenerateProjects.js
+++ JS/GenerateProjects.js
@@ -1,25 +1,39 @@
 const projectDataUrl = "../Data/ProjectsData.json"
 
-function renderProjectData(jsonString) {
-    let projectData = JSON.parse(jsonString);
-
-    let projectsContainer = document.getElementById("ProjectsContainer");
+function GenerateProjectData(jsonString) {
+    clearProjectsContainer();
     
+    let input = document.getElementById("SearchBar").value;
+
+    console.log(input);
+    
+    let projectData = JSON.parse(jsonString);
+    let projectsContainer = document.getElementById("ProjectsContainer");
     projectData.Projects.forEach(Project => {
-        //Make project section
-        let projectSection = document.createElement("section");
-        projectsContainer.appendChild(projectSection);
-        
-        //Generate title and content
-        GenerateElement("div", "projects__title", Project.Title, projectSection);
-        GenerateElement("div", "projects__content", Project.Content, projectSection);
-        
-        //Generate buttons
-        Project.Buttons.forEach(Button => {
-            GenerateButton("a", Button.ButtonClass, Button.ButtonText, Button.ButtonHref, projectSection);
-        })
+        if (input === Project.Title || input === "") {
+            console.log(input)
+            //Make project section
+            let projectSection = document.createElement("section");
+            projectsContainer.appendChild(projectSection);
+
+            //Generate title and content
+            GenerateElement("div", "projects__title", Project.Title, projectSection);
+            GenerateElement("div", "projects__content", Project.Content, projectSection);
+
+            //Generate buttons
+            Project.Buttons.forEach(Button => {
+                GenerateButton("a", Button.ButtonClass, Button.ButtonText, Button.ButtonHref, projectSection);
+            })   
+        }
     })
 }
 
-// makeAjaxCall("GET", projectDataUrl, renderProjectData);
-makeAjaxCall("GET", projectDataUrl, renderProjectData);
+function clearProjectsContainer() {
+    document.getElementById("ProjectsContainer").innerHTML = "";
+}
+
+function renderProjectData() {
+    makeAjaxCall("GET", projectDataUrl, GenerateProjectData);   
+}
+
+renderProjectData();
